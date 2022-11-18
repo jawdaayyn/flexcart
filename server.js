1fc5f2a914eb58b6612f6ab1e5d114adfe3a4ce8
unknown <akkari_j@etna-alternance.net>
2022-11-18 15:48:41 +0100

    ajt route home

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/users", usersRouter);
 
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Bienvenue dans votre interface FlexCart" });
+});
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`SERVER IS RUNNING ON PORT ${PORT}`);
